f4af87c88934125d8d619be87e56faee8e942b30
lsmin1021 <dltmdals826@gmail.com>
2021-09-30 21:09:43 +0900

    Required Permission 기능 수정

--- app/src/main/java/com/example/mypixel/activity/SplashActivity.kt
+++ app/src/main/java/com/example/mypixel/activity/SplashActivity.kt
@@ -1,17 +1,23 @@
 package com.example.mypixel.activity
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mypixel.BuildConfig
 import com.example.mypixel.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
         checkPermissions()
     }
 
@@ -23,17 +29,53 @@ class SplashActivity : AppCompatActivity() {
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
             } else {
-                // TODO: Show button that can request permissions again.
-                finish()
+                if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                    showPermissionPopup()
+                }
+                else{
+                    showSettingsPopup()
+                }
             }
         }
     }
+
+    private fun showPermissionPopup(){
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.activity_splash,null)
+        val alertDialog = AlertDialog.Builder(this)
+            .setMessage("앱을 실행하려면 카메라 권한이 필요합니다")
+            .setPositiveButton("ok"){
+                dialog, which -> checkPermissions()
+            }
+            .setNeutralButton("no",null)
+            .create()
+
+        alertDialog.setView(view)
+        alertDialog.show()
+    }
+    private fun showSettingsPopup(){
+        val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.activity_splash,null)
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            .setData(Uri.parse("package:"+ BuildConfig.APPLICATION_ID))
+
+        val alertDialog = AlertDialog.Builder(this)
+            .setMessage("앱을 실행하려면 카메라 권한을 설정해주세요")
+            .setPositiveButton("ok"){
+                    dialog, which -> startActivity(intent)
+            }
+            .setNeutralButton("no",null)
+            .create()
+
+        alertDialog.setView(view)
+        alertDialog.show()
+    }
+
 
     private fun checkPermissions() {
         if (areAllPermissionsAllowed()) {
             return
         }
-
         requestPermissions(PERMISSIONS_REQUIRED, PERMISSION_REQUEST_CODE)
     }
 
@@ -43,7 +85,6 @@ class SplashActivity : AppCompatActivity() {
                 return false
             }
         }
-
         return true
     }
 
